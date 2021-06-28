2da90fe92f7e99306e3697f56d27d68cc7e4cf90
augustomarsola <gugumarsola@hotmail.com>
2021-06-28 17:24:27 -0300

    Add project files.

---
+++ src/DevIO.Business/Core/Models/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DevIO.Business.Core.Models
+{
+    public abstract class Entity //abstratra pois ele será uma classe mãe ele só fornecerá
+    {
+        protected Entity() //Protect pois apenas a classe acessará o método
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; } //Utilizando um Guid para gerar ID's aleatórias sempre
+    }
+}
