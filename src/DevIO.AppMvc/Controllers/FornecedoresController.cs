75cee5263f068a70d37c5c338de937749fcb9ef9
augustomarsola <gugumarsola@hotmail.com>
2021-08-03 19:04:15 -0300

    Implementando as regras de servico de Produto

--- src/DevIO.AppMvc/Controllers/FornecedoresController.cs
+++ src/DevIO.AppMvc/Controllers/FornecedoresController.cs
@@ -12,44 +12,6 @@ namespace DevIO.AppMvc.Controllers
 {
     public class FornecedoresController : Controller
     {
-        private readonly IFornecedorService _fornecedorService;
-
-        public FornecedoresController()
-        {
-            _fornecedorService = new FornecedorService(new FornecedorRepository(), new EnderecoRepository());
-        }
-
-        public async Task<ActionResult> Index()
-        {
-            var fornecedor = new Fornecedor()
-            {
-                ////Testando a validação com campos errados
-                //Nome = "",
-                //Documento = "11111",
-                //Endereco = new Endereco(),
-                //TipoFornecedor = TipoFornecedor.PessoaFisica,
-                //Ativo = true
-
-                ////Testando a validação com campos corretos
-                Nome = "Marsola",
-                Documento = "23058089652",
-                Endereco = new Endereco 
-                {
-                    Logradouro = "Rua Teste",
-                    Numero = "123",
-                    Complemento = "Teste",
-                    Bairro = "Teste",
-                    Cep = "12345678",
-                    Cidade = "Teste",
-                    Estado = "Teste"
-                },
-                TipoFornecedor = TipoFornecedor.PessoaFisica,
-                Ativo = true
-            };
-
-            await _fornecedorService.Adicionar(fornecedor);
-
-            return new EmptyResult();
-        }
+        
     }
 }
