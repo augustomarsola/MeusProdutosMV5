f2b5a536959486f76020c23e2ad7df1e84e6c4fc
augustomarsola <gugumarsola@hotmail.com>
2021-08-03 16:48:31 -0300

    Implementando serviços de negócios

---
+++ src/DevIO.Business/Core/Services/BaseService.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using DevIO.Business.Core.Models;
+
+namespace DevIO.Business.Core.Services
+{
+    public abstract class BaseService //Abstrato pois só será referênciado, não poderá ser incluido novas funções
+    {
+        protected bool ExecutarValidacao<TV, TE>(TV validacao, TE entidade) where TV: AbstractValidator<TE> where TE: Entity
+        {
+            var validator = validacao.Validate(entidade);
+
+            return validator.IsValid;
+        }
+    }
+}
---
+++ src/DevIO.Business/Models/Fornecedores/Services/FornecedorService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using DevIO.Business.Core.Services;
+using DevIO.Business.Models.Fornecedores.Validations;
+
+namespace DevIO.Business.Models.Fornecedores.Services
+{
+    public class FornecedorService : BaseService, IFornecedorService
+    {
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IEnderecoRepository _enderecoRepository;
+
+        public FornecedorService(IFornecedorRepository fornecedorRepository, IEnderecoRepository enderecoRepository)
+        {
+            _fornecedorRepository = fornecedorRepository;
+            _enderecoRepository = enderecoRepository;
+        }
+
+        public async Task Adicionar(Fornecedor fornecedor)
+        {
+            if (!ExecutarValidacao(new FornecedorValidation(), fornecedor) || !ExecutarValidacao(new EnderecoValidation(), fornecedor.Endereco)) return;
+
+            if (await FornecedorExistente(fornecedor)) return;
+
+            await _fornecedorRepository.Adicionar(fornecedor);
+        }
+
+        public async Task Atualizar(Fornecedor fornecedor)
+        {
+            if (!ExecutarValidacao(new FornecedorValidation(), fornecedor)) return;
+
+            if (await FornecedorExistente(fornecedor)) return;
+
+            await _fornecedorRepository.Atualizar(fornecedor);
+        }
+
+        public async Task Remover(Guid id)
+        {
+            var fornecedor = await _fornecedorRepository.ObterFornecedorProdutosEndereco(id);
+
+            if (fornecedor.Produtos.Any()) return;
+
+            if (fornecedor.Endereco != null)
+            {
+                await _enderecoRepository.Remover(fornecedor.Endereco.Id);
+            }
+
+            await _fornecedorRepository.Remover(id);
+        }
+
+        public async Task AtualizarEndereco(Endereco endereco)
+        {
+            if (!ExecutarValidacao(new EnderecoValidation(), endereco)) return;
+
+            await _enderecoRepository.Atualizar(endereco);
+        }
+
+        public async Task<bool> FornecedorExistente(Fornecedor fornecedor)
+        {
+            var forncedorAtual = await _fornecedorRepository.Buscar(f => f.Documento == fornecedor.Documento && f.Id != fornecedor.Id);
+
+            return forncedorAtual.Any();
+        }
+
+        public void Dispose() //Não ficar com o objeto alocado na memória
+        {
+            _fornecedorRepository?.Dispose(); //O ? evita que ocorra o errl de Null Reference Excepcion
+            _enderecoRepository?.Dispose();
+        }
+    }
+}
