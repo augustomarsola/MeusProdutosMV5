f25b4678e7349f26c44d5fc6c1399cf7461606ab
augustomarsola <gugumarsola@hotmail.com>
2021-08-05 18:33:53 -0300

    Correção no produto service

--- src/DevIO.Business/Models/Produtos/Services/ProdutoService.cs
+++ src/DevIO.Business/Models/Produtos/Services/ProdutoService.cs
@@ -17,14 +17,14 @@ namespace DevIO.Business.Models.Produtos.Services
 
         public async Task Adicionar(Produto produto)
         {
-            if (!ExecutarValidacao(new ProdutoValidation, produto)) return;
+            if (!ExecutarValidacao(new ProdutoValidation(), produto)) return;
 
             await _produtoRepository.Adicionar(produto);
         }
 
         public async Task Atualizar(Produto produto)
         {
-            if (!ExecutarValidacao(new ProdutoValidation, produto)) return;
+            if (!ExecutarValidacao(new ProdutoValidation(), produto)) return;
 
             await _produtoRepository.Atualizar(produto);
         }
